1f9de47b25e342d1c8feac6733eb0cf61ee91c04
Inna Balaba <inna.balaba@ecloudvalley.com>
2024-03-26 20:57:42 +0800

    add header icons

--- components/layout/Header.vue
+++ components/layout/Header.vue
@@ -1,7 +1,19 @@
 <template>
-<div class="bg-primary h-16  flex flex-row items-center px-8">
+<div class="bg-primary text-white h-16  flex flex-row justify-between items-center px-8">
     <div class="w-1/2">
-        <p class="text-white">Frontend Development Challenge #1</p>
+        <p>Frontend Development Challenge #1</p>
+    </div>
+    <div>
+        <div class="flex flex-row gap-4">
+            <Icon
+                icon="mdi:bell"
+                height="26"
+            />
+            <Icon
+                icon="octicon:feed-person-16"
+                height="26"
+            />
+        </div>
     </div>
 </div>
 </template>
